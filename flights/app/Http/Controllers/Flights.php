2deffd223750c18be4c771b8351f1f98ce2a4239
DanielMomo <daniel_set1988@hotmail.com>
2020-11-01 16:48:22 -0300

    Listar todos

--- flights/app/Http/Controllers/Flights.php
+++ flights/app/Http/Controllers/Flights.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class Flights extends Controller
 {
     //
+
+    function all()
+    {
+        return Http::get('http://prova.123milhas.net/api/flights')->body();
+    }
 }
